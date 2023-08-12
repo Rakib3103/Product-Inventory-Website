fa637bc75b69981a1886c0fa165dd50ce266219f
Rakib <mazharul.islam.rakib@g.bracu.ac.bd>
2023-08-12 14:08:04 +0600

    Homepage design

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,13 +1,31 @@
-import React from "react"
-import "./homepage.css"
+import React, { useState } from "react";
+import "./homepage.css";
 
 const Homepage = () => {
-    return (
-        <div className="homepage">
-            <h1>This is my homepage</h1>
-            <div className="button">Logout</div>
-        </div>
-    )
-}
+  const [note, setNote] = useState("");
 
-export default Homepage
+  const handleNoteChange = (e) => {
+    setNote(e.target.value);
+  };
+
+  return (
+    <div className="homepage">
+      <div className="header">
+        <h1>This is my homepage</h1>
+        <div className="calendar">Calendar Component Here</div>
+      </div>
+      <h2>Welcome, User!</h2>
+      <div className="content">
+        <textarea
+          className="notepad"
+          placeholder="Write your notes here..."
+          value={note}
+          onChange={handleNoteChange}
+        />
+        <div className="button">Logout</div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
