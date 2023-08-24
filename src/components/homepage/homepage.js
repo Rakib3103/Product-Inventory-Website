4af8ee7ecc3fbed497b1c8c187dd6d330e5a3e49
Rakib <mazharul.islam.rakib@g.bracu.ac.bd>
2023-08-25 04:24:51 +0600

    Major Changes

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import "./homepage.css";
-
+import React, { useState } from 'react';
+import './styles.css';  // Make sure you have a file named homepage.css in the same directory for styling.
 
 const Homepage = () => {
+  // Existing State Management and Handlers
   const [note, setNote] = useState("");
 
   const handleNoteChange = (e) => {
@@ -12,23 +12,90 @@ const Homepage = () => {
   const navigateToStatistics = () => {
     window.location.href = "/statistics";
   };
+  const navigateTologin = () => {
+    window.location.href = "/login";
+  };
 
+
+
+  
+
+  // Rendered JSX
   return (
-    <div className="homepage">
-      <div className="header">
-        <h1>This is my homepage</h1>
-        <div className="calendar">Calendar</div>
-      </div>
-      <h2>Welcome, User!</h2>
-      <div className="content">
-        <textarea
-          className="notepad"
-          placeholder="Write your notes here..."
-          value={note}
-          onChange={handleNoteChange}
-        />
-        <button onClick={navigateToStatistics}>View Statistics</button> {/* This is the new button */}
-        <div className="button">Logout</div>
+    <div className="container">
+      {/* Sidebar */}
+      <aside className="sidebar">
+        <h2>SIDEBAR</h2>
+        <div className="inbox" onClick={() => console.log('Inbox clicked')}>Inbox</div>
+        <div className="category" onClick={() => console.log('Category clicked')}>Category</div>
+        <div className="settings" onClick={() => console.log('Settings clicked')}>Settings</div>
+      </aside>
+
+      {/* Dashboard */}
+      <main className="dashboard">
+        <header>
+          <h1>DASHBOARD</h1>
+          <button onClick={navigateToStatistics}>View Statistics</button>
+          <button onClick={navigateTologin}>Logout</button>
+        </header>
+
+
+        <div className="grocery">
+          <h2>Grocery List</h2>
+          <div className="Add-Grocery-List">
+            <input type="text" id="grocery-input" placeholder="Add here" />
+            <button onClick={() => console.log('Add Grocery')}>Add Grocery</button>
+          </div>
+          <ul id="grocery-list">
+            {/* The grocery list items will be added dynamically using JavaScript */}
+          </ul>
+        </div>
+
+        <div className="products">
+          <h2>Products</h2>
+          <div className="Add-Product-List">
+            <input type="text" id="product-name" placeholder="Product Name" />
+            <input type="date" id="expiry-date" />
+            <input type="number" id="quantity" placeholder="Quantity" />
+            <button onClick={() => console.log('Add Product')}>Add Product</button>
+          </div>
+          <table id="product-table">
+            <tr>
+              <th>Product-Name</th>
+              <th>Expiry-Date</th>
+              <th>Quantity</th>
+              <th>Category</th>
+            </tr>
+          </table>
+        </div>
+
+        <div className="reminder">
+          <h2>Reminder List</h2>
+          <div className="Add-reminder">
+            <input type="text" id="reminder-input" placeholder="Enter a reminder" />
+            <button onClick={() => console.log('Add Reminder')}>Add Reminder</button>
+          </div>
+          <ul id="reminder-list">
+            {/* The reminder items will be added dynamically using JavaScript */}
+          </ul>
+        </div>
+      </main>
+
+      {/* Original Homepage JSX */}
+      <div className="homepage">
+        <div className="header">
+
+        </div>
+        <div className="content">
+          <textarea
+            className="notepad"
+            placeholder="Write your notes here..."
+            value={note}
+            onChange={handleNoteChange}
+          />
+          
+          {/* <button onClick={navigateTologin}>Logout</button> */}
+        </div>
       </div>
     </div>
   );
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,41 +1,49 @@
-import React, {useState} from "react"
-import "./login.css"
-import axios from "axios"
-import { useHistory } from "react-router-dom"
-const Login = ( { setLoginUser } ) => {
-    const history = useHistory()
-    const [user, setUser] = useState({
-        email:"",
-        password:""
-    })
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./login.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
-    const handleChange = e => {
-        const { name, value } = e.target
-        setUser({
-            ...user,
-            [name]: value
-        })
-    }
+const Login = ({ setLoginUser }) => {
+  const history = useHistory();
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
 
-    const login = () => {
-        axios.post("http://localhost:9002/login", user)
-        .then(res => {
-            alert(res.data.message)
-            setLoginUser(res.data.user)
-            history.push("/")
-        })
-    }
-    return (
-        <div className="login">
-            {console.log(user)}
-            <h1>Login</h1>
-            <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
-            <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your Password"></input>
-            <div className="button" onClick={login}>Login</div>
-            <div>or</div>
-            <div className="button" onClick={() => history.push("/register")}>Register</div>
-        </div>
-    )
-}
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value
+    });
+  };
 
-export default Login
+  const login = () => {
+    axios.post("http://localhost:9002/login", user)
+      .then(res => {
+        alert(res.data.message);
+        setLoginUser(res.data.user);
+        history.push("/");
+      });
+  };
+
+  return (
+    <div className="login">
+      {console.log(user)}
+      <h1>Login</h1>
+      <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email" />
+      <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your Password" />
+      <div className="button" onClick={login}>Login</div>
+      <div>or</div>
+      <div className="button" onClick={() => history.push("/register")}>Register</div>
+    </div>
+  );
+};
+
+// Adding PropTypes
+Login.propTypes = {
+  setLoginUser: PropTypes.func.isRequired
+};
+
+export default Login;
