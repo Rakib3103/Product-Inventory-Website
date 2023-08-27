848dbd242689fb27ba078eb19a6fd94224204ccc
Rakib <mazharul.islam.rakib@g.bracu.ac.bd>
2023-08-28 00:37:49 +0600

    Calender changes

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -173,63 +173,75 @@ const Homepage = () => {
           <button onClick={navigateTologin}>Logout</button>
         </header>
 
+      <div className="calendar-notepad-container">
 
         <div className="calendar">
-          
-  <h2>Calendar</h2>
-  <div className="month">
-    <ul>
-      <li className="prev">&#10094;</li>
-      <li className="next">&#10095;</li>
-      <li>
-        August<br />
-        <span style={{ fontSize: '18px' }}>2021</span>
-      </li>
-    </ul>
-  </div>
-  <ul className="weekdays">
-    <li>Mo</li>
-    <li>Tu</li>
-    <li>We</li>
-    <li>Th</li>
-    <li>Fr</li>
-    <li>Sa</li>
-    <li>Su</li>
-  </ul>
-  <ul className="days">
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
-    <li>11</li>
-    <li>12</li>
-    <li>13</li>
-    <li>14</li>
-    <li>15</li>
-    <li>16</li>
-    <li>17</li>
-    <li>18</li>
-    <li>19</li>
-    <li>20</li>
-    <li>21</li>
-    <li>22</li>
-    <li>23</li>
-    <li>24</li>
-    <li>25</li>
-    <li>26</li>
-    <li>27</li>
-    <li>28</li>
-    <li>29</li>
-    <li>30</li>
-    <li>31</li>
-  </ul>
-</div>
+          <h2>Calendar</h2>
+          <div className="month">
+            <ul>
+              <li className="prev">&#10094;</li>
+              <li className="next">&#10095;</li>
+              <li>
+                August<br />
+                <span style={{ fontSize: '18px' }}>2021</span>
+              </li>
+            </ul>
+          </div>
+          <ul className="weekdays">
+            <li>Mo</li>
+            <li>Tu</li>
+            <li>We</li>
+            <li>Th</li>
+            <li>Fr</li>
+            <li>Sa</li>
+            <li>Su</li>
+          </ul>
+          <ul className="days">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+            <li>11</li>
+            <li>12</li>
+            <li>13</li>
+            <li>14</li>
+            <li>15</li>
+            <li>16</li>
+            <li>17</li>
+            <li>18</li>
+            <li>19</li>
+            <li>20</li>
+            <li>21</li>
+            <li>22</li>
+            <li>23</li>
+            <li>24</li>
+            <li>25</li>
+            <li>26</li>
+            <li>27</li>
+            <li>28</li>
+            <li>29</li>
+            <li>30</li>
+            <li>31</li>
+          </ul>
+        </div>
+
+        <div className="notepad-container">
+          <textarea
+            className="notepad"
+            placeholder="Write your notes here..."
+            value={note}
+            onChange={handleNoteChange}
+          />
+          <button onClick={handleSaveNote}>Save Note</button>
+        </div>
+      </div>
+
 
       <div className="grocery">
         <h2>Grocery List</h2>
@@ -324,7 +336,7 @@ const Homepage = () => {
         <div className="header">
 
         </div>
-        <div className="notepad-container">
+        {/* <div className="notepad-container">
           <textarea
             className="notepad"
             placeholder="Write your notes here..."
@@ -332,7 +344,7 @@ const Homepage = () => {
             onChange={handleNoteChange}
           />
           <button onClick={handleSaveNote}>Save Note</button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
